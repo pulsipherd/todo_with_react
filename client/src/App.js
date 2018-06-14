66d5d389543ca66a0bd86e22c4a8d5e50ff687da
Deneb Pulsipher <pulsipherd@gmail.com>
2018-06-14 17:02:06 -0600

    work without database access

--- client/src/App.js
+++ client/src/App.js
@@ -7,20 +7,34 @@ class App extends Component {
 
   componentDidMount() {
     // TODO make a call to our rails server to get items
+    fetch('/api/items')
+      .then(res => res.json())
+      .then( todos => this.setState({ todos}) )
   }
 
   addItem = (name) => {
     // TODO make api call to rails server to add item
-    // TODO update state
+    const { todos } = this.state;
+    // Generate random id
+    const id = Math.floor((1 + Math.random()) * 0x1000).toString()
+    this.setState({ todos: [...todos, { id, name }] });
   }
 
   updateTodo = (id) => {
     // TODO make api call to update todo
+    let todos = this.state.todos.map(t => {
+      if (t.id === id)
+        return { ...t, complete: !t.complete }
+      return t;
+    });
     // TODO update state
+    this.setState({ todos });
   }
 
   deleteTodo = (id) => {
     // TODO make api call to delete todo
+    const { todos } = this.state;
+    this.setState({ todos: todos.filter( t => t.id !== id ) })
     // TODO remove it from state
   }
 
